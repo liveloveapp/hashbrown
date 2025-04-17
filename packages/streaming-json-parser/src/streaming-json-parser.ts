8f73ee53dcceb639f21f92813c95d98e22aab4ee
Benjamin Taylor <ben@liveloveapp.com>
2025-04-17 11:35:50 -0500

    Cleanup

--- packages/streaming-json-parser/src/streaming-json-parser.ts
+++ packages/streaming-json-parser/src/streaming-json-parser.ts
@@ -8,39 +8,15 @@ export function parse(data: string) {
 TODO:
 - build parser
   - "find chunks of 'schema' in a stream of bytes that will be a whole JSON document
-    - custom parsing via bytes and textdecoder (see example below)
+    - custom parsing via bytes and textdecoder?
     - investigate https://github.com/dominictarr/JSONStream (archived in 2018)
       - mine for ideas and for to implement an event emitter in pipe()
   - should take a schema as an input
     - should validate schema on 'start up'
       - can the schema have loops?
-      - 
 - build stream event handler
   - when a chunk matching a schema is found, let consumer know
-- figure out how to interact with streams from other sources (i.e. open AI completion streams)
-  - TransformStream to wrap a returned stream?
 */
-
-// Just pass data straight through for now
-export const ParseStream = new TransformStream({
-  transform(chunk, controller) {
-    console.log(chunk);
-    controller.enqueue(chunk);
-  },
-});
-
-export class ParserTransform extends Stream.Transform {
-  constructor() {
-    super();
-  }
-  override _transform(chunk: any, encoding: any, callback: any) {
-    // var obj = this;
-    // obj.hash.update(chunk); //this is synchronous.
-    // obj.push(chunk);
-    console.log('Transform: ' + chunk);
-    callback();
-  }
-}
 
 export async function* AsyncParserIterable(
   iterable: AsyncIterable<string>,
