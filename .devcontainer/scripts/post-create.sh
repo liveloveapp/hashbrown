8b7156d13e0af5035079bde8a153f6f1a0b72740
U.G. Wilson <ugwilson@gmail.com>
2025-04-19 18:19:01 +0000

    cleanup

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -3,13 +3,6 @@
 # Change cache to avoid filling devcontainer storage
 npm config set cache /workspaces/hashbrown/.npm-cache
 
-# Setup environment.ts file if it doesn't exist
-if [ ! -f /workspaces/hashbrown/samples/smart-home/server/src/environment.ts ]; then
-    echo "Creating environment.ts file"
-    touch /workspaces/hashbrown/samples/smart-home/server/src/environment.ts
-    echo "export const OPEN_AI_API_KEY=sk-proj-1234567890" >> /workspaces/hashbrown/samples/smart-home/server/src/environment.ts
-fi
-
 # Install Correct Node Version
 # nvm install
 # nvm use
