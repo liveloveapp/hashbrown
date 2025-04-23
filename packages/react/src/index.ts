19473a383f7efa44f08dd04022e33797fc76e7ec
U.G. Wilson <ugwilson@gmail.com>
2025-04-23 04:13:21 +0000

    woops

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,4 @@
-export { Chat } from '@hashbrownai/core';
+export { Chat, exposeComponent, s } from '@hashbrownai/core';
 export * from './lib/create-tool.fn';
 export * from './lib/hashbrown-provider';
 export * from './lib/use-chat';
