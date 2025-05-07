703076f0a905d97e62313e9bef2b469af1137478
Benjamin Taylor <ben@liveloveapp.com>
2025-05-07 17:07:01 -0500

    Should be parsing more betterer now

--- packages/core/src/streaming-json-parser/test/client.ts
+++ packages/core/src/streaming-json-parser/test/client.ts
@@ -17,36 +17,36 @@ import { s } from '../../schema';
       title: s.string('glossary.title'),
       GlossDiv: s.object('GlossDiv', {
         title: s.string('GlossDiv.title'),
-        // GlossList: s.streaming.array(
-        //   'GlossDiv.GlossList',
-        //   s.object('', {
-        //     ID: s.string('GlossList.ID'),
-        //     SortAs: s.string('GlossList.SortAs'),
-        //     GlossTerm: s.string('GlossList.GlossTerm'),
-        //     Acronym: s.string('GlossList.Acronym'),
-        //     GlossDef: s.object('GlossList.GlossDef', {
-        //       para: s.string('GlossDef.para'),
-        //       GlossSeeAlso: s.array('GlossDef.GlossSeeAlso', s.string('')),
-        //     }),
-        //     GlossSee: s.string('GlossList.GlossSee'),
-        //     ExampleSentences: s.streaming.array(
-        //       'GlossList.ExampleSentences',
-        //       s.string('ExampleSentence'),
-        //     ),
-        //   }),
-        // ),
-        // SynonymList: s.streaming.array(
-        //   'GlossDiv.SynonymList',
-        //   s.object('Synonym', {
-        //     ID: s.string('Synonym.ID'),
-        //     GlossTerm: s.string('Synonym.GlossTerm'),
-        //     Acronym: s.string('Synonym.Acronym'),
-        //     SynonymDef: s.object('Synonym.SynonymDef', {
-        //       word: s.string('Synonym.word'),
-        //       meaning: s.string('SynonymDef.meaning'),
-        //     }),
-        //   }),
-        // ),
+        GlossList: s.streaming.array(
+          'GlossDiv.GlossList',
+          s.object('', {
+            ID: s.string('GlossList.ID'),
+            SortAs: s.string('GlossList.SortAs'),
+            GlossTerm: s.string('GlossList.GlossTerm'),
+            Acronym: s.string('GlossList.Acronym'),
+            GlossDef: s.object('GlossList.GlossDef', {
+              para: s.string('GlossDef.para'),
+              GlossSeeAlso: s.array('GlossDef.GlossSeeAlso', s.string('')),
+            }),
+            GlossSee: s.string('GlossList.GlossSee'),
+            ExampleSentences: s.streaming.array(
+              'GlossList.ExampleSentences',
+              s.string('ExampleSentence'),
+            ),
+          }),
+        ),
+        SynonymList: s.streaming.array(
+          'GlossDiv.SynonymList',
+          s.object('Synonym', {
+            ID: s.string('Synonym.ID'),
+            GlossTerm: s.string('Synonym.GlossTerm'),
+            Acronym: s.string('Synonym.Acronym'),
+            SynonymDef: s.object('Synonym.SynonymDef', {
+              word: s.string('Synonym.word'),
+              meaning: s.string('SynonymDef.meaning'),
+            }),
+          }),
+        ),
         anyOfListWithDiscriminator: s.streaming.array(
           'GlossDiv.anyOfListDiscriminator',
           s.anyOf('anyOfListWithDiscriminator', [
@@ -91,6 +91,7 @@ import { s } from '../../schema';
       // To see how things are changing in a dynamic way, clear the console before
       // parsing update
       console.clear();
+      console.log('new chunk');
       console.log(JSON.stringify(data, null, 4));
     }
     console.log('Socket ended.');
