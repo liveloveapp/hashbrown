c5fb11a2a39e397d8270afa282cd883fbf7990d8
Benjamin Taylor <ben@liveloveapp.com>
2025-06-03 11:04:22 -0500

    Support function-specific logging levels

--- packages/core/src/logger/logger.ts
+++ packages/core/src/logger/logger.ts
@@ -1,55 +1,95 @@
-export type LoggerLevel =
-  | 'trace'
-  | 'debug'
-  | 'info'
-  | 'warn'
-  | 'error'
-  | 'none';
-
-const LOGGER_LEVEL_RANKING: LoggerLevel[] = [
-  'trace',
-  'debug',
-  'info',
-  'warn',
-  'error',
-  'none',
-];
+export const TRACE_LEVEL = 0;
+export const DEBUG_LEVEL = 1;
+export const INFO_LEVEL = 2;
+export const WARN_LEVEL = 3;
+export const ERROR_LEVEL = 4;
+export const NONE_LEVEL = 5;
 
 export class Logger {
-  logPathsEnabled: { [name: string]: LoggerLevel } = {};
+  logPathsEnabled: { [name: string]: number } = {};
 
-  allLevel: LoggerLevel;
+  allLevel: number;
 
-  constructor(logPathsEnabled: { [name: string]: LoggerLevel }) {
+  constructor(logPathsEnabled: { [name: string]: number }) {
     this.logPathsEnabled = logPathsEnabled;
 
-    this.allLevel = logPathsEnabled['all'];
+    this.allLevel = logPathsEnabled['all'] ?? NONE_LEVEL;
   }
 
-  trace(args: any) {
-    if (
-      LOGGER_LEVEL_RANKING.indexOf(this.allLevel) >=
-        LOGGER_LEVEL_RANKING.indexOf('trace') ||
-      // TODO: how to get the log name from args?
-      LOGGER_LEVEL_RANKING.indexOf(this.logPathsEnabled['']) >=
-        LOGGER_LEVEL_RANKING.indexOf('trace')
-    )
-      console.trace(args);
+  for(loggerName: string) {
+    return {
+      trace: (args: any) => {
+        if (
+          this.allLevel <= TRACE_LEVEL ||
+          (this.logPathsEnabled[loggerName] &&
+            this.logPathsEnabled[loggerName] <= TRACE_LEVEL)
+        ) {
+          console.trace(args);
+        }
+      },
+      debug: (args: any) => {
+        if (
+          this.allLevel <= DEBUG_LEVEL ||
+          (this.logPathsEnabled[loggerName] &&
+            this.logPathsEnabled[loggerName] <= DEBUG_LEVEL)
+        ) {
+          console.debug(args);
+        }
+      },
+      info: (args: any) => {
+        if (
+          this.allLevel <= INFO_LEVEL ||
+          (this.logPathsEnabled[loggerName] &&
+            this.logPathsEnabled[loggerName] <= INFO_LEVEL)
+        ) {
+          console.info(args);
+        }
+      },
+      warn: (args: any) => {
+        if (
+          this.allLevel <= WARN_LEVEL ||
+          (this.logPathsEnabled[loggerName] &&
+            this.logPathsEnabled[loggerName] <= ERROR_LEVEL)
+        ) {
+          console.error(args);
+        }
+      },
+      error: (args: any) => {
+        if (
+          this.allLevel <= ERROR_LEVEL ||
+          (this.logPathsEnabled[loggerName] &&
+            this.logPathsEnabled[loggerName] <= TRACE_LEVEL)
+        ) {
+          console.trace(args);
+        }
+      },
+    };
   }
 
-  debug(args: any) {
-    if (this.allLevel === 'debug' || this.logPathsEnabled) console.debug(args);
-  }
+  // trace(args: any) {
+  //   if (
+  //     LOGGER_LEVEL_RANKING.indexOf(this.allLevel) >=
+  //       LOGGER_LEVEL_RANKING.indexOf('trace') ||
+  //     // TODO: how to get the log name from args?
+  //     LOGGER_LEVEL_RANKING.indexOf(this.logPathsEnabled['']) >=
+  //       LOGGER_LEVEL_RANKING.indexOf('trace')
+  //   )
+  //     console.trace(args);
+  // }
 
-  info(args: any) {
-    if (this.allLevel === 'info' || this.logPathsEnabled) console.info(args);
-  }
+  // debug(args: any) {
+  //   if (this.allLevel === 'debug' || this.logPathsEnabled) console.debug(args);
+  // }
 
-  warn(args: any) {
-    if (this.allLevel === 'warn' || this.logPathsEnabled) console.warn(args);
-  }
+  // info(args: any) {
+  //   if (this.allLevel === 'info' || this.logPathsEnabled) console.info(args);
+  // }
 
-  error(args: any) {
-    if (this.allLevel === 'error' || this.logPathsEnabled) console.error(args);
-  }
+  // warn(args: any) {
+  //   if (this.allLevel === 'warn' || this.logPathsEnabled) console.warn(args);
+  // }
+
+  // error(args: any) {
+  //   if (this.allLevel === 'error' || this.logPathsEnabled) console.error(args);
+  // }
 }
