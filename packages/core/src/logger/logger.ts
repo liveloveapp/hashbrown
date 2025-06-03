2ceeebe888b8fb6dd1d1948eabb0d66593dee09b
Benjamin Taylor <ben@liveloveapp.com>
2025-06-03 11:41:50 -0500

    Remove commented out old code

--- packages/core/src/logger/logger.ts
+++ packages/core/src/logger/logger.ts
@@ -65,31 +65,4 @@ export class Logger {
       },
     };
   }
-
-  // trace(args: any) {
-  //   if (
-  //     LOGGER_LEVEL_RANKING.indexOf(this.allLevel) >=
-  //       LOGGER_LEVEL_RANKING.indexOf('trace') ||
-  //     // TODO: how to get the log name from args?
-  //     LOGGER_LEVEL_RANKING.indexOf(this.logPathsEnabled['']) >=
-  //       LOGGER_LEVEL_RANKING.indexOf('trace')
-  //   )
-  //     console.trace(args);
-  // }
-
-  // debug(args: any) {
-  //   if (this.allLevel === 'debug' || this.logPathsEnabled) console.debug(args);
-  // }
-
-  // info(args: any) {
-  //   if (this.allLevel === 'info' || this.logPathsEnabled) console.info(args);
-  // }
-
-  // warn(args: any) {
-  //   if (this.allLevel === 'warn' || this.logPathsEnabled) console.warn(args);
-  // }
-
-  // error(args: any) {
-  //   if (this.allLevel === 'error' || this.logPathsEnabled) console.error(args);
-  // }
 }
