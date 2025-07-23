5685de917f4e542bfb48062a13566c2c8927a0cf
Mike Ryan <mike.ryan52@gmail.com>
2025-07-23 13:46:40 -0700

    feat: Get some initial styles for a player turn

--- samples/spotify/client/src/app/game-loop/player-turn.ts
+++ samples/spotify/client/src/app/game-loop/player-turn.ts
@@ -1,14 +1,49 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 
 @Component({
   selector: 'spot-player-turn',
   template: `
-    <div>
-      <h1>Player Turn: {{ player() }}</h1>
+    <div class="player-turn-preview" [class.hidden]="!hidden()">
+      <span class="player-name">{{ player() }}</span>
+      <button (click)="hidden.set(false)">Start Turn</button>
+    </div>
+    <div [class.hidden]="hidden()">
       <ng-content></ng-content>
     </div>
+  `,
+  styles: `
+    .player-turn-preview {
+      position: fixed;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 10px;
+      width: max(200vh, 200vw);
+      height: max(200vh, 200vw);
+      background-color: var(--sunshine-yellow);
+      color: rgba(0, 0, 0, 0.6);
+      transition: all 0.5s ease-in-out;
+      transform-origin: center;
+      top: 50%;
+      left: 50%;
+      margin-left: min(-100vw, -100vh);
+      margin-top: min(-100vw, -100vh);
+      border-radius: max(100vh, 100vw);
+      opacity: 1;
+
+      @starting-style {
+        transform: scale(0);
+      }
+    }
+
+    .player-turn-preview.hidden {
+      opacity: 0;
+      transform: scale(0);
+      pointer-events: none;
+    }
   `,
 })
 export class PlayerTurnComponent {
   player = input.required<string>();
+  hidden = signal(true);
 }
--- samples/spotify/client/src/app/game-loop/song-picker-view.component.ts
+++ samples/spotify/client/src/app/game-loop/song-picker-view.component.ts
@@ -49,6 +49,10 @@ export class SongPickerViewComponent {
 
       Each message will contain the user's query, and the active
       constraint. You should use the tools query songs from Spotify.
+
+      You must show the <spot-song> component to the user if there
+      are valid songs to pick from, otherwise the user cannot
+      select a song.
     `,
     components: [
       exposeComponent(MarkdownComponent, {
