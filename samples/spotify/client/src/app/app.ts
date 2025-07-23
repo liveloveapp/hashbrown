5a707865fa6132009e5e6b34db4e3abcdc54cb4e
Mike Ryan <mike.ryan52@gmail.com>
2025-07-23 07:15:58 -0700

    chore: Prepare for the web dev challenge

---
+++ samples/spotify/client/src/app/app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'spot-root',
+  template: `<h1>Welcome spotify-angular-client</h1> `,
+  styles: ``,
+})
+export class App {}
