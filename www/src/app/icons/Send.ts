99a093e4c15a693a4e989076f120c4ab58a841f6
Mike Ryan <mike.ryan52@gmail.com>
2025-08-17 02:36:06 -0700

    feat(www): Implement the design for the Open Source section

--- www/src/app/icons/Send.ts
+++ www/src/app/icons/Send.ts
@@ -10,7 +10,7 @@ import { Component, input } from '@angular/core';
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="#000000"
+      stroke="currentColor"
       stroke-width="1"
       stroke-linecap="round"
       stroke-linejoin="round"
