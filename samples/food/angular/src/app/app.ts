5f453b360818a6e742f8f92a8d61902b425fd398
Mike Ryan <mike.ryan52@gmail.com>
2025-11-05 10:56:41 -0800

    samples: Scaffold out the food app

---
+++ samples/food/angular/src/app/app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-root',
+  template: `<h1>Welcome food-angular</h1> `,
+  styles: ``,
+})
+export class App {}
---
+++ samples/food/server/src/main.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello API' });
+});
+
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
+});
