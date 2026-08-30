cdc9584fad8ecd44bb819333df4e4b27d3653ede
Brian Love <brian@liveloveapp.com>
2026-08-30 01:19:20 -0700

    fix(openai): harden AG-UI stream mapping

--- packages/openai/jest.config.ts
+++ packages/openai/jest.config.ts
@@ -7,5 +7,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/openai',
-  testPathIgnorePatterns: ['(?!.*integration).*\\.ts$'],
+  testPathIgnorePatterns: ['/integration\\.spec\\.ts$'],
 };
